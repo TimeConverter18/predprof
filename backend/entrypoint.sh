179fdda3bf221f2fbcbda7a90403651c42cc7ebf
palenhame <glazovartemiy@gmail.com>
2026-04-15 18:39:55 +0300

    feat: fix websocket

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -10,4 +10,13 @@ else
     echo "Данные уже загружены или dump.json не найден, пропускаем"
 fi
 
+python manage.py shell -c "
+from users.models import User
+if not User.objects.filter(email='admin@mail.ru').exists():
+    User.objects.create_superuser(email='admin@mail.ru', username='admin', password='12345678')
+    print('Суперпользователь создан')
+else:
+    print('Суперпользователь уже существует')
+"
+
 daphne -b 0.0.0.0 -p 8000 --root-path /api olympiad.asgi:application
